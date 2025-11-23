46885431894b1e85725af7848c8fe99d2f77dc47
Stan-21 <shung951@gmail.com>
2025-11-23 05:22:56 -0800

    Created simple enemy projectile

--- Assets/Scripts/Enemy/GameEntity.cs
+++ Assets/Scripts/Enemy/GameEntity.cs
@@ -8,18 +8,18 @@ public abstract class GameEntity : MonoBehaviour
     public float moveSpeed;
     public float targetFrequency; // The closer to the target frequency, the more damage the enemy takes
 
-    public float attackRate; // How many seconds need to pass for an attack to happen
-    public float attackTimer; // How many seconds since the last attack
+    public float attackRate; // How many seconds need to pass for an attack to happen / could sub as projectile duration?
+    public float attackTimer = 0; // How many seconds since the last attack
 
     [SerializeField] public Rigidbody2D rb;
     [SerializeField] public SpriteRenderer spriteRenderer;
-
-    public void init(float hp, float atk, float move, float freq)
+    public void init(float hp, float atk, float move, float freq, float atkRate)
     {
         health = hp;
         attackDmg = atk;
         moveSpeed = move;
         targetFrequency = freq;
+        attackRate = atkRate;
     }
 
     public void takeDamage(float dmg, float atkFrequency)
---
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class ProjectileBehaviour : GameEntity
+{
+    public Vector3 targetPosition;
+    public GameObject owner;
+
+    private void Start()
+    {
+        targetPosition = new Vector3(5, 4, 0);
+        SetLifeTime(attackRate);
+    }
+
+    private void Update()
+    {
+        tick();
+    }
+    public void tick()
+    {
+        Vector3 direction = targetPosition - transform.position;
+        direction = direction.normalized;
+        rb.linearVelocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject gameObject = other.gameObject;
+        Player player = gameObject.GetComponent<Player>();
+        if (player == null)
+        {
+            return;
+        }
+
+        player.damage(attackDmg);
+        Destroy(gameObject);
+
+    }
+
+    public void SetLifeTime(float time)
+    {
+        StartCoroutine(Expire(time));
+    }
+
+    IEnumerator Expire(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
